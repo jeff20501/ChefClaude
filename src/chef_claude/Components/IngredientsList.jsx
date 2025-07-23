2dda191436ec2e170b14b9a72f5fd2bf104be4c5
jeff20501 <jeffkimmuna@gmail.com>
2025-07-23 22:46:25 +0300

    .

--- src/chef_claude/Components/IngredientsList.jsx
+++ src/chef_claude/Components/IngredientsList.jsx
@@ -8,13 +8,13 @@ export function IngredientsList(props){
     return(
         <section>
             <h2>Ingredients on hand:</h2>
-            <ul className="ingredients-list" aria-live="polite">{props.ingredientsListItems}</ul>
+            <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
             {props.ingredients.length > 3 ? <div className="get-recipe-container">
                 <div>
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button onClick={()=>props.toggleRecipeShown()}>Get a recipe</button>
+                <button onClick={props.getRecipe}>Get a recipe</button>
             </div>: null}
         </section>
 )}
--- src/chef_claude/Components/Main.jsx
+++ src/chef_claude/Components/Main.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 import { IngredientsList } from "./IngredientsList"
 import { ClaudeRecipe } from "./ClaudeRecipe"
+import { getRecipeFromMistral } from "./AI"
 export function Main(){
     const [ingredients, setIngredients]=useState(["all the main spices", "pasta", "ground beef", "tomato paste"])
-    
-    const [recipeShown, setRecipeShown] = useState(false)
 
-    function toggleRecipeShown() {
-        setRecipeShown(prevShown => !prevShown)
+    const [recipe, setRecipe] = useState("")
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
     }
 
     function addIngredients(formData){
@@ -18,7 +19,7 @@ export function Main(){
     }
  
     return(
-        <main className="wrapper">
+        <article className="wrapper">
             <form action={addIngredients} className="input"> 
                 <input
                 id="ingredient" 
@@ -32,10 +33,10 @@ export function Main(){
 
            {ingredients.length > 0 ? <IngredientsList 
                     ingredients={ingredients}
-                    toggleRecipeShown={toggleRecipeShown}
+                    getRecipe={getRecipe}
                 /> 
             : null}
-            {recipeShown ? <ClaudeRecipe/>:undefined}
-        </main>
+            <ClaudeRecipe recipe={recipe}/>
+        </article>
     )
 } 
