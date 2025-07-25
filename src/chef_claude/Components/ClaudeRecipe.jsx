95967f0c7b42e7107316c3de328d0aa59e4e4391
jeff20501 <jeffkimmuna@gmail.com>
2025-07-25 15:30:06 +0300

    .

--- src/chef_claude/Components/ClaudeRecipe.jsx
+++ src/chef_claude/Components/ClaudeRecipe.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown'
 export function ClaudeRecipe(props){
 return(
         <section className="suggested-recipe-container" aria-live="polite">
-            <h2>Chef Claude Recommends:</h2>
+            {props.recipe?<h2>Chef Claude Recommends:</h2>:null}
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
 )}
